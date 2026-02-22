e7bfc73ed44e33a6a14f0e901cd28363d010ee4d
daniboycam <danielbatista1908112@gmail.com>
2026-02-22 20:44:45 -0300

    feat: implement event persistence and fix missing remote field mapping.

--- api/src/main/java/com/eventostec/api/domain/service/EventService.java
+++ api/src/main/java/com/eventostec/api/domain/service/EventService.java
@@ -3,8 +3,10 @@ package com.eventostec.api.domain.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,6 +27,9 @@ public class EventService {
     @Autowired
     private AmazonS3 s3Client;
 
+    @Autowired
+    private EventRepository repository;
+
     public Event createEvent(EventRequestDTO data){
         String imgUrl = null;
         if(data.image() != null){
@@ -37,8 +42,9 @@ public class EventService {
         newEvent.setEventUrl(data.eventUrl());
         newEvent.setDate(new Date(data.date()));
         newEvent.setImgUrl(imgUrl);
+        newEvent.setRemote(data.remote());
 
-        return newEvent;
+        return repository.save(newEvent);
     }
 
     private String uploadImg(MultipartFile multipartFile){

70ae917703ff2db00a02caba5e15fe63c2d437b5
daniboycam <danielbatista1908112@gmail.com>
2026-02-25 07:36:34 -0300

    feat: implement dynamic event filtering by title, city, uf and date range.

--- api/src/main/java/com/eventostec/api/domain/service/EventService.java
+++ api/src/main/java/com/eventostec/api/domain/service/EventService.java
@@ -35,6 +35,9 @@ public class EventService {
     @Autowired
     private EventRepository repository;
 
+    @Autowired
+    private AddressService addressService;
+
     public Event createEvent(EventRequestDTO data){
         String imgUrl = null;
         if(data.image() != null){
@@ -49,13 +52,45 @@ public class EventService {
         newEvent.setImgUrl(imgUrl);
         newEvent.setRemote(data.remote());
 
-        return repository.save(newEvent);
+        repository.save(newEvent);
+
+        if(data.remote() != null){
+            this.addressService.createAddress(data, newEvent);
+        }
+        return newEvent;
     }
 
     public List<EventResponseDTO> getUpcomingEvents(int page, int size){
         Pageable pageable = PageRequest.of(page, size);
         Page<Event> eventsPage = this.repository.findUpcomingEvents(new Date(), pageable);
-        return eventsPage.map(event -> new EventResponseDTO(event.getId(), event.getTitle(), event.getDescription(), event.getDate(), "", "", event.getRemote(), event.getEventUrl(), event.getImgUrl()))
+        return eventsPage.map(event -> new EventResponseDTO(
+                        event.getId(),
+                        event.getTitle(),
+                        event.getDescription(),
+                        event.getDate(),
+                        event.getAddress() != null ? event.getAddress().getCity() : "",
+                        event.getAddress() != null ? event.getAddress().getUf() : "",
+                        event.getRemote(),
+                        event.getEventUrl(),
+                        event.getImgUrl())
+                )
+                .stream().toList();
+    }
+
+    public List<EventResponseDTO> getFilteredEvents(int page, int size, String title, String city, String uf, Date startDate, Date endDate){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = this.repository.findFilteredEvents(new Date(), title, city, uf, startDate, endDate, pageable);
+        return eventsPage.map(event -> new EventResponseDTO(
+                        event.getId(),
+                        event.getTitle(),
+                        event.getDescription(),
+                        event.getDate(),
+                        event.getAddress() != null ? event.getAddress().getCity() : "",
+                        event.getAddress() != null ? event.getAddress().getUf() : "",
+                        event.getRemote(),
+                        event.getEventUrl(),
+                        event.getImgUrl())
+                )
                 .stream().toList();
     }
 
